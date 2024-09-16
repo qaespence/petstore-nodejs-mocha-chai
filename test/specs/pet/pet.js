b11f050482659398f2ddea4e8ccc9e909d8f40a1
Eric Spence <ericspence@gmail.com>
2024-09-16 06:21:36 -0700

    Adding tests for POST /v2/pet

---
+++ test/specs/pet/pet.js
@@ -0,0 +1,60 @@
+'use strict'
+const utils = require('../../helpers/utils.js')
+const petApi = require('../../api/pet.js')
+const expect = require('chai').expect
+
+describe("Pet API Tests", () => {
+
+    let petsToDelete = []
+
+    before( async() => {
+        await utils.setLogFileName("pet_api")
+    })
+
+    after( async() => {
+        for(let i = 0; i < petsToDelete.length; i++) {
+            await petApi.deletePet(petsToDelete[i])
+        }
+    })
+
+    beforeEach( function() {
+        utils.logMessageToFile(this.currentTest.title)
+    })
+
+    //
+    // POST /pet/:petId
+    //
+
+    it("Test pet create", async() => {
+        let petData = await utils.generateRandomPet()
+        const addPetResponse = await petApi.addPet(petData.id, petData.category, petData.name, 
+            petData.photoUrls, petData.tags, petData.status)
+        petsToDelete.push(addPetResponse.body.id)
+
+        const testResults = await utils.multiPointVerification(addPetResponse,
+            200, [`"id":${petData.id}`, `"name":"${petData.name}"`, 
+                `"category":{"id":${petData.category.id},"name":"${petData.category.name}"}`,
+                '"photoUrls"', '"tags"', `"status":"${petData.status}"`], undefined, 
+                ['"content-type":"application/json"', '"transfer-encoding":"chunked"', '"connection":"close"',
+                '"access-control-allow-origin":"*"', '"access-control-allow-methods":"GET, POST, DELETE, PUT"',
+            '"access-control-allow-headers":"Content-Type, api_key, Authorization"'], undefined, 
+            [`"id":${petData.id}`, `"name":"${petData.name}"`, 
+                `"category":{"id":${petData.category.id},"name":"${petData.category.name}"}`,
+                '"photoUrls"', '"tags"', `"status":"${petData.status}"`])
+        expect(testResults, "Verify test results").to.equal("No mismatch values")
+    })
+
+    it("Test pet create schema", async() => {
+        let petData = await utils.generateRandomPet()
+        const addPetResponse = await petApi.addPet(petData.id, petData.category, petData.name, 
+            petData.photoUrls, petData.tags, petData.status)
+        petsToDelete.push(addPetResponse.body.id)
+
+        const testResults = utils.schemaValidation("pet", "/v2/pet", "POST",
+            addPetResponse.body, addPetResponse.header, false, true)
+        expect(testResults, "Verify test results").to.equal("No mismatch values")
+    })
+
+
+
+})
