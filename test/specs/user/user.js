415721d0aef9d51062b3a86a98f9a68543a8ae9b
qaespence <ericspence@gmail.com>
2024-10-31 09:23:03 -0700

    Added more POST /v2/user tests

--- test/specs/user/user.js
+++ test/specs/user/user.js
@@ -73,4 +73,133 @@ describe("User API Tests", () => {
         expect(testResults, "Verify test results").to.equal("No mismatch values")
     })
 
+    it("Test user create with id missing", async() => {
+        let userData = await utils.generateRandomUser()
+        const testPayload = {
+            "username": userData.username,
+            "firstName": userData.firstName,
+            "lastName": userData.lastName,
+            "email": userData.email,
+            "password": userData.password,
+            "phone": userData.phone,
+            "userStatus": userData.userStatus
+        }
+
+        const addUserResponse = await basicRequests.post("/v2/user", 
+            {"content-type":"application/json"}, testPayload)
+        usersToDelete.push(userData.username)
+
+        const testResults = await utils.multiPointVerification(addUserResponse,
+            200, [`"code":200`, `"type":"unknown"`, `"message"`], undefined, 
+                ['"content-type":"application/json"', '"transfer-encoding":"chunked"', '"connection":"close"',
+                '"access-control-allow-origin":"*"', '"access-control-allow-methods":"GET, POST, DELETE, PUT"',
+            '"access-control-allow-headers":"Content-Type, api_key, Authorization"'], undefined, 
+            [`"code":200`, `"type":"unknown"`, `"message"`])
+        expect(testResults, "Verify test results").to.equal("No mismatch values")
+    })
+
+    it("Test user create with id invalid (data type)", async() => {
+        let userData = await utils.generateRandomUser()
+        const testPayload = {
+            "id": "bad",
+            "username": userData.username,
+            "firstName": userData.firstName,
+            "lastName": userData.lastName,
+            "email": userData.email,
+            "password": userData.password,
+            "phone": userData.phone,
+            "userStatus": userData.userStatus
+        }
+
+        const addUserResponse = await basicRequests.post("/v2/user", 
+            {"content-type":"application/json"}, testPayload)
+        usersToDelete.push(userData.username)
+
+        const testResults = await utils.multiPointVerification(addUserResponse,
+            500, [`"code":500`, `"type":"unknown"`, `"message":"something bad happened"`], undefined, 
+                ['"content-type":"application/json"', '"transfer-encoding":"chunked"', '"connection":"close"',
+                '"access-control-allow-origin":"*"', '"access-control-allow-methods":"GET, POST, DELETE, PUT"',
+            '"access-control-allow-headers":"Content-Type, api_key, Authorization"'], undefined, 
+            [`"code":500`, `"type":"unknown"`, `"message":"something bad happened"`])
+        expect(testResults, "Verify test results").to.equal("No mismatch values")
+    })
+
+    it("Test user create with id invalid (-1)", async() => {
+        let userData = await utils.generateRandomUser()
+        const testPayload = {
+            "id": -1,
+            "username": userData.username,
+            "firstName": userData.firstName,
+            "lastName": userData.lastName,
+            "email": userData.email,
+            "password": userData.password,
+            "phone": userData.phone,
+            "userStatus": userData.userStatus
+        }
+
+        const addUserResponse = await basicRequests.post("/v2/user", 
+            {"content-type":"application/json"}, testPayload)
+        usersToDelete.push(userData.username)
+
+        const testResults = await utils.multiPointVerification(addUserResponse,
+            200, [`"code":200`, `"type":"unknown"`, `"message"`], undefined, 
+                ['"content-type":"application/json"', '"transfer-encoding":"chunked"', '"connection":"close"',
+                '"access-control-allow-origin":"*"', '"access-control-allow-methods":"GET, POST, DELETE, PUT"',
+            '"access-control-allow-headers":"Content-Type, api_key, Authorization"'], undefined, 
+            [`"code":200`, `"type":"unknown"`, `"message"`])
+        expect(testResults, "Verify test results").to.equal("No mismatch values")
+    })
+
+    it("Test user create with id invalid (0)", async() => {
+        let userData = await utils.generateRandomUser()
+        const testPayload = {
+            "id": 0,
+            "username": userData.username,
+            "firstName": userData.firstName,
+            "lastName": userData.lastName,
+            "email": userData.email,
+            "password": userData.password,
+            "phone": userData.phone,
+            "userStatus": userData.userStatus
+        }
+
+        const addUserResponse = await basicRequests.post("/v2/user", 
+            {"content-type":"application/json"}, testPayload)
+        usersToDelete.push(userData.username)
+
+        const testResults = await utils.multiPointVerification(addUserResponse,
+            200, [`"code":200`, `"type":"unknown"`, `"message"`], undefined, 
+                ['"content-type":"application/json"', '"transfer-encoding":"chunked"', '"connection":"close"',
+                '"access-control-allow-origin":"*"', '"access-control-allow-methods":"GET, POST, DELETE, PUT"',
+            '"access-control-allow-headers":"Content-Type, api_key, Authorization"'], undefined, 
+            [`"code":200`, `"type":"unknown"`, `"message"`])
+        expect(testResults, "Verify test results").to.equal("No mismatch values")
+    })
+
+    it("Test user create with id null", async() => {
+        let userData = await utils.generateRandomUser()
+        const testPayload = {
+            "id": null,
+            "username": userData.username,
+            "firstName": userData.firstName,
+            "lastName": userData.lastName,
+            "email": userData.email,
+            "password": userData.password,
+            "phone": userData.phone,
+            "userStatus": userData.userStatus
+        }
+
+        const addUserResponse = await basicRequests.post("/v2/user", 
+            {"content-type":"application/json"}, testPayload)
+        usersToDelete.push(userData.username)
+
+        const testResults = await utils.multiPointVerification(addUserResponse,
+            200, [`"code":200`, `"type":"unknown"`, `"message"`], undefined, 
+                ['"content-type":"application/json"', '"transfer-encoding":"chunked"', '"connection":"close"',
+                '"access-control-allow-origin":"*"', '"access-control-allow-methods":"GET, POST, DELETE, PUT"',
+            '"access-control-allow-headers":"Content-Type, api_key, Authorization"'], undefined, 
+            [`"code":200`, `"type":"unknown"`, `"message"`])
+        expect(testResults, "Verify test results").to.equal("No mismatch values")
+    })
+
 })
